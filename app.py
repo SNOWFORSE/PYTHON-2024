f295831b583a64efadaefbfbfd697ce06b80568e
SNOWFORSE <134400943+SNOWFORSE@users.noreply.github.com>
2024-05-15 08:19:10 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+print("Sabor Express\n")
+
+print("1. Cadastro Resteurante")
+print("2. Lista Resteurante")
+print("3. Ativar Restaurante")
+print("4. Sair\n")
+
+opção_escolhida = input("Escolha uma Opção: ")
+print("Você escolheu a opção" , opção_escolhida)
