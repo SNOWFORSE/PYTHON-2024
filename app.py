ebcaf1387632092b69542d2542b727f7277eda72
SNOWFORSE <134400943+SNOWFORSE@users.noreply.github.com>
2024-05-17 12:07:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,20 +21,30 @@ def finalizar_app():
     print("finalizando o app\n")
 
 
-def escolher_opçao():
-    opção_escolhida =  int (input("Escolha uma Opção: "))
-    # opção_escolhida = int(opção_escolhida) 
+def opaçao_invalida():
+    print("Opção Invalida")
+    input("Digite uma tecla para voltar ao menu principal")
 
-    if opção_escolhida == 1:
-        print("Cadastrar Restaurante")
-    elif opção_escolhida == 2:
-        print("Listar Restaurantes")
-    elif opção_escolhida == 3:
-        print("Ativar Restaurante")
-    else:
-        finalizar_app()
+
+def escolher_opçao():
+    try:
+        opção_escolhida =  int (input("Escolha uma Opção: "))
+        # opção_escolhida = int(opção_escolhida) 
+
+        if opção_escolhida == 1:
+            print("Cadastrar Restaurante")
+        elif opção_escolhida == 2:
+            print("Listar Restaurantes")
+        elif opção_escolhida == 3:
+            print("Ativar Restaurante")
+        elif opção_escolhida == 4:
+            finalizar_app()
+        else:
+            opaçao_invalida()
+
 
 def main():
+    os.system("cls")
     exibir_nome_do_programa()
     exibir_opçoes()
     escolher_opçao()
