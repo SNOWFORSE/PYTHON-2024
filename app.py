87202187e10c7c43cc243b6735ad77d017b7e829
SNOWFORSE <134400943+SNOWFORSE@users.noreply.github.com>
2024-05-17 10:41:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
 def exibir_nome_do_programa():
+
     print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
@@ -9,34 +10,34 @@ def exibir_nome_do_programa():
 ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░""")
 
-
-print("1. Cadastro Resteurante")
-print("2. Listar Resteurantes")
-print("3. Ativar Restaurante")
-print("4. Sair\n")
-
-opção_escolhida =  int (input("Escolha uma Opção: "))
-# opção_escolhida = int(opção_escolhida) 
-
+def exibir_opçoes():
+    print("1. Cadastro Resteurante")
+    print("2. Listar Resteurantes")
+    print("3. Ativar Restaurante")
+    print("4. Sair\n")
+ 
 def finalizar_app():
     os.system("cls")
     print("finalizando o app\n")
 
 
+def escolher_opçao():
+    opção_escolhida =  int (input("Escolha uma Opção: "))
+    # opção_escolhida = int(opção_escolhida) 
 
-
-
-if opção_escolhida == 1:
-    print("Cadastrar Restaurante")
-elif opção_escolhida == 2:
-    print("Listar Restaurantes")
-elif opção_escolhida == 3:
-    print("Ativar Restaurante")
-else:
-    finalizar_app()
+    if opção_escolhida == 1:
+        print("Cadastrar Restaurante")
+    elif opção_escolhida == 2:
+        print("Listar Restaurantes")
+    elif opção_escolhida == 3:
+        print("Ativar Restaurante")
+    else:
+        finalizar_app()
 
 def main():
     exibir_nome_do_programa()
+    exibir_opçoes()
+    escolher_opçao()
 
 if __name__ == "__main__":
     main()
